ece3c6ee597293d1e2a14118bcee15d1cbc12244
FranklinMonro <fbg2468@gmail.com>
2023-12-10 09:22:43 +0200

    Server and routes

---
+++ blog/posts/server/app.ts
@@ -0,0 +1,49 @@
+import { json, urlencoded } from 'body-parser';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { appLogger as log, errorHandlerLogger as errorLog } from './winstonLogger';
+
+
+class App {
+  public httpServer = express();
+
+  constructor() {
+    this.httpServer.use(json({ limit: '500mb' }));
+
+    this.httpServer.use(urlencoded({ extended: true, limit: '500mb' }));
+
+    this.httpServer.use((req: Request, res: Response, next: NextFunction) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorization, X-CSRFToken',
+      );
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, OPTIONS, DELETE');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      next();
+    });
+
+    this.httpServer.set('trust proxy', true);
+
+    this.httpServer.get('/', (req: Request, res: Response) => {
+      console.log('Welcome to Udemy_microservices_with_node_js_and_react posts API');
+      res.send('Welcome to Udemy_microservices_with_node_js_and_react posts API');
+    });
+
+    process.once('uncaughtException', (err: Error) => {
+      console.log(err.name, err.message);
+      console.log('UNCAUGHT EXCEPTION!  Shutting down...');
+      log.log('error', `uncaughtException - Error name: ${err.name}, Error message: ${err.message}, UNCAUGHT EXCEPTION!  Shutting down...`);
+      process.exit(1);
+    });
+
+    process.once('unhandledRejection', (reason: Error) => {
+      console.log(reason.name, reason.message);
+      console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+      log.log('error', `uncaughtException - Reason name: ${reason.name}, Reason message: ${reason.message}, UNHANDLED REJECTION!  Shutting down...`);
+      process.exit(1);
+    });
+  }
+}
+
+export default new App().httpServer;
---
+++ blog/posts/server/config.ts
@@ -0,0 +1,10 @@
+
+import { configLogger as log } from './/winstonLogger';
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+export {
+    PORT,
+    HOST,
+  };
